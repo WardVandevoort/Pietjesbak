20e67e680af60fcf62771885c9fe5927ac931e04
Ward Vandevoort <vandevoort.ward@gmail.com>
2019-12-11 14:02:40 +0100

    Basic layout speelscherm

--- app/src/main/java/com/example/pietjesbak/MainActivity.java
+++ app/src/main/java/com/example/pietjesbak/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pietjesbak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     String string1 = "Welkom ";
     String string2;
     String string3;
+    String error1 = "Kies een naam";
 
 
 
@@ -30,13 +32,34 @@ public class MainActivity extends AppCompatActivity {
 
             public void onClick(View v){
                 string2 = et.getText().toString();
-                
 
                 string3 = string1 + string2;
 
-                Toast.makeText (getApplicationContext(),string3, Toast.LENGTH_LONG).show();
+                if(string2.equals("") || string2.equals(" ")){
+
+                    Toast.makeText (getApplicationContext(),error1, Toast.LENGTH_LONG).show();
+
+                }
+                else{
+
+
+                    Toast.makeText (getApplicationContext(),string3, Toast.LENGTH_LONG).show();
+                    openActivity2();
+
+                }
+
             }
 
         });
+
+
     }
+
+    public void openActivity2(){
+
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+
+    }
+
 }
