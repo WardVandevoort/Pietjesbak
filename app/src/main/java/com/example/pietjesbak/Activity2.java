fa193ed084daafc71ae78128f27f7e1ea827ec52
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-01-07 19:54:29 +0100

    Layout klaar

--- app/src/main/java/com/example/pietjesbak/Activity2.java
+++ app/src/main/java/com/example/pietjesbak/Activity2.java
@@ -2,13 +2,47 @@ package com.example.pietjesbak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
+
+    TextView tv;
+    TextView tv2;
+    CheckBox check1;
+    CheckBox check2;
+    CheckBox check3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        tv = (TextView) findViewById(R.id.textView6);
+        tv2 = (TextView) findViewById(R.id.textView2);
+
+        Intent intent = getIntent();
+            Bundle extras = intent.getExtras();
+            String string2 = extras.getString("PLAYER1");
+            String string4 = extras.getString("PLAYER2");
+
+            tv.setText(string4);
+            tv2.setText(string2);
+
+        check1 = findViewById(R.id.checkBox);
+        check2 = findViewById(R.id.checkBox2);
+        check3 = findViewById(R.id.checkBox3);
+
+
+
     }
+
+
+
+
 }
--- app/src/main/java/com/example/pietjesbak/MainActivity.java
+++ app/src/main/java/com/example/pietjesbak/MainActivity.java
@@ -12,11 +12,15 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText et;
+    EditText et2;
     Button btn;
     String string1 = "Welkom ";
     String string2;
-    String string3;
-    String error1 = "Kies een naam";
+    String string3 = " en ";
+    String string4;
+    String string5;
+    String error1 = "Beide spelers moeten een naam kiezen";
+
 
 
 
@@ -26,16 +30,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         et = (EditText) findViewById(R.id.inputField);
+        et2 = (EditText) findViewById(R.id.inputField2);
         btn = (Button) findViewById(R.id.button);
 
         btn.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v){
                 string2 = et.getText().toString();
+                string4 = et2.getText().toString();
 
-                string3 = string1 + string2;
+                string5 = string1 + string2 + string3 + string4;
 
-                if(string2.equals("") || string2.equals(" ")){
+                if(string2.equals("") || string2.equals(" ") || string4.equals("") || string4.equals(" ")){
 
                     Toast.makeText (getApplicationContext(),error1, Toast.LENGTH_LONG).show();
 
@@ -43,10 +49,11 @@ public class MainActivity extends AppCompatActivity {
                 else{
 
 
-                    Toast.makeText (getApplicationContext(),string3, Toast.LENGTH_LONG).show();
+                    Toast.makeText (getApplicationContext(),string5, Toast.LENGTH_LONG).show();
                     openActivity2();
 
                 }
+
 
             }
 
@@ -58,6 +65,10 @@ public class MainActivity extends AppCompatActivity {
     public void openActivity2(){
 
         Intent intent = new Intent(this, Activity2.class);
+        Bundle extras = new Bundle();
+        extras.putString("PLAYER1",string2);
+        extras.putString("PLAYER2",string4);
+        intent.putExtras(extras);
         startActivity(intent);
 
     }
